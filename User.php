e35d8ab5bfe27a1b196d2aa0dc5a38d966461cef
AsthaSharma1 <31702181+AsthaSharma1@users.noreply.github.com>
2017-12-17 10:51:43 -0500

    Week 7 and 9
    HW

---
+++ User.php
@@ -0,0 +1,143 @@
+<?php
+class User{
+	function User(){
+		$servername = "sql2.njit.edu";
+		$username = "as2865";
+		$password = "embroil52";
+		$dbname = "as2865";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Successful Connection <br>";
+	$this->connection = $conn;
+	}
+	function displayAllUser(){
+		$sql = "SELECT * FROM accounts";
+		$result = $this->connection->query($sql);
+		echo '<table style="border-style: dotted;">';
+		echo '<tr>';
+		echo '<th colspan="8" style="text-align: center;font-weight: bold">All Users Information</th></tr>';
+		echo '<tr>';
+		echo '<th style="text=align:center;font-weight:bold">ID</th>';
+		echo '<th style="text=align:center;font-weight:bold">Email</th>';
+		echo '<th style="text=align:center;font-weight:bold">First Name</th>';
+		echo '<th style="text=align:center;font-weight:bold">Last Name</th>';
+		echo '<th style="text=align:center;font-weight:bold">Phone</th>';
+		echo '<th style="text=align:center;font-weight:bold">Birthday</th>';
+		echo '<th style="text=align:center;font-weight:bold">Gender</th>';
+		echo '<th style="text=align:center;font-weight:bold">Password</th>';
+		echo '</tr>';
+		while($row = $result->fetch_assoc()) {
+			echo "<tr>";
+			echo "<td>".$row["id"]."</td>";
+			echo "<td>".$row["email"]."</td>";
+			echo "<td>".$row["fname"]."</td>";
+			echo "<td>".$row["lname"]."</td>";
+			echo "<td>".$row["phone"]."</td>";
+			echo "<td>".$row["birthday"]."</td>";
+			echo "<td>".$row["gender"]."</td>";
+			echo "<td>".$row["password"]."</td>";
+			echo "</tr>";
+    	}
+    	echo "</table>";
+	}
+	function deleteUser($id){
+		$sql = "SELECT * FROM accounts where id = '$id'";
+		$result = $this->connection->query($sql);
+		echo "Deleted this record: <br>";
+		echo '<table style="border-style: dashed;">';
+		echo '<th style="text=align:center;font-weight:bold">ID</th>';
+		echo '<th style="text=align:center;font-weight:bold">Email</th>';
+		echo '<th style="text=align:center;font-weight:bold">First Name</th>';
+		echo '<th style="text=align:center;font-weight:bold">Last Name</th>';
+		echo '<th style="text=align:center;font-weight:bold">Phone</th>';
+		echo '<th style="text=align:center;font-weight:bold">Birthday</th>';
+		echo '<th style="text=align:center;font-weight:bold">Gender</th>';
+		echo '<th style="text=align:center;font-weight:bold">Password</th>';
+		echo '</tr>';
+		while($row = $result->fetch_assoc()) {
+			echo "<tr>";
+			echo "<td>".$row["id"]."</td>";
+			echo "<td>".$row["email"]."</td>";
+			echo "<td>".$row["fname"]."</td>";
+			echo "<td>".$row["lname"]."</td>";
+			echo "<td>".$row["phone"]."</td>";
+			echo "<td>".$row["birthday"]."</td>";
+			echo "<td>".$row["gender"]."</td>";
+			echo "<td>".$row["password"]."</td>";
+			echo "</tr>";
+    	}
+    	echo "</table>";
+		$sql = "DELETE FROM accounts where id = '$id'";
+		$result = $this->connection->query($sql);
+	}
+	function insertUser($id, $email, $fname, $lname, $phone, $birthday, $gender, $password){
+		$sql = "INSERT into accounts values('$id', '$email', '$fname', '$lname', '$phone', '$birthday', '$gender', '$password')";
+		$result = $this->connection->query($sql);
+		$sql = "SELECT * FROM accounts where id = '$id'";
+		$result = $this->connection->query($sql);
+		echo "Inserted this record: <br>";
+		echo '<table style="border-style: solid;">';
+		echo '<th style="text=align:center;font-weight:bold">ID</th>';
+		echo '<th style="text=align:center;font-weight:bold">Email</th>';
+		echo '<th style="text=align:center;font-weight:bold">First Name</th>';
+		echo '<th style="text=align:center;font-weight:bold">Last Name</th>';
+		echo '<th style="text=align:center;font-weight:bold">Phone</th>';
+		echo '<th style="text=align:center;font-weight:bold">Birthday</th>';
+		echo '<th style="text=align:center;font-weight:bold">Gender</th>';
+		echo '<th style="text=align:center;font-weight:bold">Password</th>';
+		echo '</tr>';
+		while($row = $result->fetch_assoc()) {
+			echo "<tr>";
+			echo "<td>".$row["id"]."</td>";
+			echo "<td>".$row["email"]."</td>";
+			echo "<td>".$row["fname"]."</td>";
+			echo "<td>".$row["lname"]."</td>";
+			echo "<td>".$row["phone"]."</td>";
+			echo "<td>".$row["birthday"]."</td>";
+			echo "<td>".$row["gender"]."</td>";
+			echo "<td>".$row["password"]."</td>";
+			echo "</tr>";
+    	}
+    	echo "</table>";
+	}
+	function updatePassword($id, $newPassword){
+		$sql = "update accounts set password = '$newPassword' where id = '$id'";
+		$result = $this->connection->query($sql);
+		$sql = "SELECT * FROM accounts where id = '$id'";
+		$result = $this->connection->query($sql);
+		echo "Updated this record: <br>";
+		echo '<table style="border-style: double;">';
+		echo '<th style="text=align:center;font-weight:bold">ID</th>';
+		echo '<th style="text=align:center;font-weight:bold">Email</th>';
+		echo '<th style="text=align:center;font-weight:bold">First Name</th>';
+		echo '<th style="text=align:center;font-weight:bold">Last Name</th>';
+		echo '<th style="text=align:center;font-weight:bold">Phone</th>';
+		echo '<th style="text=align:center;font-weight:bold">Birthday</th>';
+		echo '<th style="text=align:center;font-weight:bold">Gender</th>';
+		echo '<th style="text=align:center;font-weight:bold">Password</th>';
+		echo '</tr>';
+		while($row = $result->fetch_assoc()) {
+			echo "<tr>";
+			echo "<td>".$row["id"]."</td>";
+			echo "<td>".$row["email"]."</td>";
+			echo "<td>".$row["fname"]."</td>";
+			echo "<td>".$row["lname"]."</td>";
+			echo "<td>".$row["phone"]."</td>";
+			echo "<td>".$row["birthday"]."</td>";
+			echo "<td>".$row["gender"]."</td>";
+			echo "<td>".$row["password"]."</td>";
+			echo "</tr>";
+    	}
+    	echo "</table>";
+	}
+}
+$user = new User();
+$user->displayAllUser();
+$user->deleteUser(450);
+$user->insertUser(450, 'hello@gmail.com', 'Astha', 'Sharma', '-2230', '1992-02-28', 'female', 'password');
+$user->updatePassword(2, '222');
+?>
---
+++ dp.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "sql2.njit.edu";
+$username = "as2865";
+$password = "embroil52";
+$dbname = "as2865";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully <br>";
+$sql = "SELECT * FROM accounts where 'user ID' < 6";
+$result = $conn->query($sql);
+echo $result->num_rows.' results <br>';
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>Name</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["user ID"]."</td><td>".$row["name"]." </td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+?>
